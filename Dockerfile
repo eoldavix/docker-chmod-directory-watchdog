d3e6b87b73fb1ad914ea14cdc3444ddcdf83bc58
David Carrera del Castillo <david.carrera@civir.es>
2018-08-08 10:42:49 +0200

    Possibility to indicate the permissions of the new folder

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:2-alpine
 
+ENV PERMS=0777
+
 VOLUME [ "/data" ]
 
 WORKDIR /usr/src/watchdog
--- app.py
+++ app.py
@@ -12,18 +12,20 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
+try: 
+    perms = int(os.environ['PERMS'], 8)
+except Exception as e:
+    perms = int('0777', 8)
 
 class MyHandler(events.FileSystemEventHandler):
     def on_created(self, event):
         if type(event) == events.DirCreatedEvent:
             try:
-                os.chmod(event.src_path, 0777)
-                msg = "Directory {} created. Changed perms to 0777.".format(
-                    event.src_path)
+                os.chmod(event.src_path, perms)
+                msg = "Directory {} created. Changed perms to {}.".format(
+                    event.src_path, oct(perms))
                 logging.info(msg)
             except Exception as e:
-                print type(e)
-                print e
                 msg = "Error while changing permissions to {}".format(
                     event.src_path)
                 logging.error(msg)
